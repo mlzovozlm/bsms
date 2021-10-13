aaee55890166ee5b1e6041037df5ac3316ce2e71
Bao Anh Luu <mlzovozlm@gmail.com>
2021-10-13 22:18:30 +0700

    update
    update usercontroller.java, login_page.js, register_page.js

--- src/main/java/com/bsms/bsms/controller/UserController.java
+++ src/main/java/com/bsms/bsms/controller/UserController.java
@@ -1,2 +1,19 @@
 //login
 //register
+(getmap - '/login')
+String loginPage(Model model){
+return "login_page"
+}
+(postmap - '/login')
+String login(User user, String error){
+return "home_page"
+}
+
+(getmap - '/register')
+String registerPage(Model model){
+return "register_page"
+}
+(postmap - '/register')
+String register(User user){
+return "redirect:/registerSuccess_page"
+}
